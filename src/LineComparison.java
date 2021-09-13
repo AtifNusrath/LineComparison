d3aafb78a55ebe2f490c78e159bfa498a18b2305
atif00 <mohammedatif90@gmail.com>
2021-09-13 16:05:47 +0530

    ADD Lenght Feature

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,6 +1,7 @@
-public class UC1 {
+public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Problem on Mater Branch");
-
+        Line line = new Line(8,3,3,-2);
+        line.length();
     }
 }
