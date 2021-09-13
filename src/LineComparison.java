432f2d29e7dd9c632e1bbf0efc1098b85ebb9f3a
AtifNusrath <89371249+AtifNusrath@users.noreply.github.com>
2021-09-13 16:17:36 +0530

    Refactor LineLenght problem

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -1,6 +1,6 @@
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Problem on Mater Branch");
+        System.out.println("Welcome to Line Comparison Problem");
         Line line = new Line(8,3,3,-2);
         line.length();
     }
